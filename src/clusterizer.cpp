56062b75e7a1bf99f63984eb645f485c510d9b9c
cloud441 <kevin.guillet@epita.fr>
2020-10-22 12:09:32 +0200

    [ARCHI]: Start the architecture of the project

---
+++ src/clusterizer.cpp
@@ -0,0 +1,17 @@
+#include "clusterizer.hpp"
+
+
+
+
+
+
+
+int main(int argc, char **argv)
+{
+    if (argc > 10)
+    {
+        std::cerr << "test";
+    }
+
+    return 0;
+}
---
+++ src/clusterizer.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <iostream>
+
+//#include "opt_parser.hpp"
